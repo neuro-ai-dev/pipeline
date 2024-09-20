8f26c5f3f89690c2de596edc5f0b067fdc1f3966
Ross Gray <ross@mystic.ai>
2024-09-20 08:41:15 +0000

    Support Turbo Reg for Cog pipelines

--- pipeline/console/container/convert.py
+++ pipeline/console/container/convert.py
@@ -1,6 +1,8 @@
 import subprocess
 from argparse import Namespace
+from pathlib import Path
 
+import docker
 import yaml
 
 from pipeline.container import docker_templates
@@ -59,6 +61,12 @@ def convert_cog(pipeline_name: str) -> PipelineConfig:
         # capture_output=True,
     )
 
+    # squash original Cog image into a single layer (only really needed for
+    # Turbo Registry but we do it here so we have the flexibility to use it
+    # later or not)
+    _print("Squashing Cog image...", "INFO")
+    _squash_image(cog_image_name)
+
     # Generate a pipeline config. Note that most of these fields will not be
     # used when wrapping a Cog pipeline
     config = PipelineConfig(
@@ -80,3 +88,49 @@ def convert_cog(pipeline_name: str) -> PipelineConfig:
         readme="README.md",
     )
     return config
+
+
+def _squash_image(image_name) -> None:
+    """Squashes the given image into a single layer by re-building from a newly
+    generated Dockerfile
+    """
+    docker_client: docker.DockerClient = docker.from_env(timeout=600)
+    image = docker_client.images.get(image_name)
+    working_dir = image.attrs.get("Config", {}).get("WorkingDir", "/src")
+    env_vars = image.attrs.get("Config", {}).get("Env", [])
+    entrypoint = image.attrs.get("Config", {}).get("Entrypoint", ["/sbin/tini", "--"])
+    cmd = image.attrs.get("Config", {}).get("Cmd", ["python", "-m", "cog.server.http"])
+    dockerfile_str = docker_templates.cog_dockerfile_template.format(
+        base_image=image_name,
+        workdir=working_dir,
+        env="ENV " + "\nENV ".join(env_vars),
+        entrypoint=str(entrypoint).replace("'", '"'),
+        cmd=str(cmd).replace("'", '"'),
+    )
+    dockerfile_path = Path("./pipeline.dockerfile")
+    dockerfile_path.write_text(dockerfile_str)
+
+    docker_api_client = docker.APIClient()
+    generator = docker_api_client.build(
+        path="./",
+        dockerfile=dockerfile_path.absolute(),
+        rm=True,
+        decode=True,
+        platform="linux/amd64",
+        tag=image_name,
+    )
+    docker_image_id = None
+    while True:
+        try:
+            output = generator.__next__()
+            if "aux" in output:
+                docker_image_id = output["aux"]["ID"]
+            if "stream" in output:
+                _print(output["stream"].strip("\n"))
+            if "errorDetail" in output:
+                raise Exception(output["errorDetail"])
+        except StopIteration:
+            _print("Docker image build complete.")
+            break
+
+    _print(f"Built and squashed Cog image {image_name} : {docker_image_id}")
